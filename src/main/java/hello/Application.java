9ab5e1cd6a7119e5b408234986e140bfe3f995c3
Samuel Zaragoza <alexzm1@gmail.com>
2017-03-20 20:27:11 -0600

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,18 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by alexzm1 on 3/20/17.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
+
+
